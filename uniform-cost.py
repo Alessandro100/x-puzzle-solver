c28f56d945c86c895871cc90b20b992e4cca95d9
Alessandro <kalessandro14@gmail.com>
2020-11-16 14:48:47 -0500

    algorithm optimizations

--- uniform-cost.py
+++ uniform-cost.py
@@ -4,39 +4,46 @@ from xpuzzle import XPuzzle
 
 ## what to do next:
 # clean up your code to fit the submission document
+# timer
 
 class UniformCost:
     def __init__(self, initial_state):
         print("class init")
-        self.close_list = PriorityQueue()
         self.open_list = PriorityQueue()
         self.goal_node = ()
         self.initial_state = initial_state
 
     def run(self):
+        uniform_cost_search_file = open("0_ucs_search.txt", "w")
+        uniform_cost_search_file.write("0 0 0 " + self.initial_state + "\n")
+        uniform_cost_search_file.close()
         start = time.time()
         goal_state = "1 2 3 4 5 6 7 0"
         self.set_state_open_list(self.initial_state, 0) # nodes that need to be visited (priority queue or dictionairy)
 
         print('start loop')
+        close_list = {}
         while not self.open_list.empty():
             # finds the smallest cost path and visits it
             current_node = self.open_list.get()
             current_node_prev_key = current_node[2]
             current_node_key = current_node[1]
             current_node_cost = current_node[0]
-            print("0 0 " + current_node_key)
-            
-            # checks if goal state was reached
-            if(current_node_key == goal_state):
-                self.goal_node = current_node
-                break
 
-            # we need to add this key to the closed list
-            self.close_list.put((current_node))
+            if current_node_key not in close_list or close_list[current_node_key] > current_node_cost:
+                close_list[current_node_key] = current_node_cost
 
-            # now we need to add the new nodes to the open list
-            open_list = self.set_state_open_list(current_node_key, current_node_cost)
+                print("0 "+ str(current_node_cost)+ " 0 " + current_node_key)
+                with open("0_ucs_search.txt", "a") as ucs_search:
+                    ucs_search.write("0 "+ str(current_node_cost) +" 0 " + current_node_key+"\n")
+                
+                # checks if goal state was reached
+                if(current_node_key == goal_state):
+                    self.goal_node = current_node
+                    break
+
+                # now we need to add the new nodes to the open list
+                open_list = self.set_state_open_list(current_node_key, current_node_cost)
 
         end = time.time()
         print('I finished running in: ' + str(end - start) + " seconds")
@@ -97,6 +104,15 @@ class UniformCost:
             print("COST: " + str(self.goal_node[0]))
             print("KEY: " + str(self.goal_node[1]))
             print("PREV: " + str(self.goal_node[2]))
+            print("This is the path it took")
+            prev = self.goal_node[2]
+            # while(prev != self.initial_state):
+            #     print(prev)
+            #     prev = self.open_list.get
 
-algo = UniformCost("1 0 3 6 5 2 7 4")
+#1 0 3 6 5 2 7 4
+#6 3 4 7 1 2 5 0
+#algo = UniformCost("1 0 3 6 5 2 7 4")
+algo = UniformCost("6 3 4 7 1 2 5 0")
+
 algo.run()
